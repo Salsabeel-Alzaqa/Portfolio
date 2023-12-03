af87a2ec038ff19f44f9c2c919571221450baec3
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-12-03 19:19:08 +0200

    done ContactForm

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,13 +1,15 @@
 'use client';
 import { useState } from 'react';
-import styles from './ContactForm.module.css'
+import styles from './ContactForm.module.css';
 
 const ContactForm = () => {
-  const [formData, setFormData] = useState({
+  const initialFormData = {
     name: '',
     email: '',
     message: '',
-  });
+  };
+
+  const [formData, setFormData] = useState(initialFormData);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -17,49 +19,65 @@ const ContactForm = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Form Data:', formData);
-    setFormData({
-      name: '',
-      email: '',
-      message: '',
-    });
+    try {
+      const response = await fetch('https://formspree.io/f/mrgwwwzz', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        setFormData(initialFormData);
+        console.log('Form submitted successfully!');
+      } else {
+        console.error('Form submission failed.');
+      }
+    } catch (error) {
+      console.error('Error submitting the form:', error);
+    }
   };
 
-    return (
-        <div className={styles['form-container']}>
-            <form onSubmit={handleSubmit} className={styles.form}>
-                <input
-                    type="text"
-                    name="name"
-                    value={formData.name}
-                    onChange={handleChange}
-                    placeholder="Name"
-                    required
-                    className={styles.name}
-                />
-                <input
-                    type="email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                    placeholder="Email"
-                    required
-                    className={styles.email}
-                />
-                <textarea
-                    name="message"
-                    value={formData.message}
-                    onChange={handleChange}
-                    placeholder="Message"
-                    required
-                    className={styles.message}
-                ></textarea>
-                <button type="submit" className={styles.submit}>Submit</button>
-            </form>
-        </div>
-    );
+  return (
+    <div className={styles['form-container']}>
+      <form
+        onSubmit={handleSubmit}
+        className={styles.form}
+      >
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Name"
+          required
+          className={styles.name}
+        />
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          placeholder="Email"
+          required
+          className={styles.email}
+        />
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          placeholder="Message"
+          required
+          className={styles.message}
+        ></textarea>
+        <button type="submit" className={styles.submit}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default ContactForm;
