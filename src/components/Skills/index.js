f61db66e5e455d4a6ef243daf59ce754963cbe67
salsabeel alzaqa <salsabeel.alzaqa@gmail.com>
2023-12-06 00:07:21 +0200

    done

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -1,41 +1,40 @@
+'use client'
 import styles from './skills.module.css';
-const programming = [
-    {
-        label: 'HTML', percent: 90
-    },
-    {
-        label: 'CSS', percent: 80
-    },
-    {
-        label: 'JavaScript', percent: 75
-    },
-    {
-        label: 'React', percent: 72
-    },
-    {
-        label: 'C++', percent: 75
-    },
-    {
-        label: 'Java', percent: 60
-    },
-]
+import { useInView } from 'react-intersection-observer';
+
+const ProgrammingSkills = [
+  { label: 'HTML', percent: 90 },
+  { label: 'CSS', percent: 80 },
+  { label: 'JavaScript', percent: 75 },
+  { label: 'React', percent: 72 },
+  { label: 'C++', percent: 75 },
+  { label: 'Java', percent: 60 },
+];
+
+const Skill = ({ label, percent }) => {
+  const [ref, inView] = useInView();
+
+  return (
+    <div ref={ref} className={styles['programming-skill']}>
+      <div className={styles['skill-label']}>
+        <h6>{label}</h6>
+        <h6>{percent}%</h6>
+      </div>
+      <div className={`${styles['skill-progress']} ${inView ? styles.active : ''}`}>
+        <div style={{ width: inView ? `${percent}%` : '0' }} className={styles['progress-bar']}></div>
+      </div>
+    </div>
+  );
+};
+
 const Skills = () => {
-    return (
-        <div className={styles['skill-container']}>
-                {
-                    programming.map((skill,index) => (
-                        <div className={styles['programming-skill']} key={index}>
-                            <div className={styles['skill-label']}>
-                                <h6>{skill.label}</h6>
-                                <h6>{skill.percent}%</h6>
-                            </div>
-                            <div className={styles['skill-progress']}>
-                                <div style={{ width: `${skill.percent}%` }} className={styles['progress-bar']}></div>
-                            </div>
-                        </div>
-                    ))
-                }
-        </div>
-    );
-}
-export default Skills
+  return (
+    <div className={styles['skill-container']}>
+      {ProgrammingSkills.map((skill, index) => (
+        <Skill key={index} label={skill.label} percent={skill.percent} />
+      ))}
+    </div>
+  );
+};
+
+export default Skills;
